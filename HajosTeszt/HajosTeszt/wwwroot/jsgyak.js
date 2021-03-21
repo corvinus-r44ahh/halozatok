9b2accef1e3f2c2e58f1827a58aba86114846b0e
Tóbiás Tóth <tobias.toth@stud.uni-corvinus.hu>
2021-03-21 13:11:32 +0100

    js

---
+++ HajosTeszt/HajosTeszt/wwwroot/jsgyak.js
@@ -0,0 +1,23 @@
+﻿
+window.onload = function () {
+    console.log("start");
+
+    let hova = document.getElementById("ide");
+    for (var s = 0; s < 10; s++) {
+
+        let sor = document.createElement("div");
+        sor.classList.add("sor");
+        hova.appendChild(sor);
+
+        for (var o = 0; o < 10; o++) {
+            let szam = document.createElement("div");
+            szam.classList.add("elem");
+            szam.innerText = (s + 1) * (o + 1);
+            sor.appendChild(szam); 
+            szam.style.color = "white";
+            szam.style.background = `rgb(${255 / 10 * s}, 0, ${255 / 10 * o})`
+             
+
+        }
+    }
+}
