fc2327777f8561c5fb8fee72e789d3f96624f6fb
Tóbiás Tóth <tobias.toth@stud.uni-corvinus.hu>
2021-04-16 13:19:05 +0200

    kerdeslista

---
+++ HajosTeszt/HajosTeszt/Controllers/TesztController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HajosTeszt.Controllers
+{
+    public class TesztController : ControllerBase
+    {
+        [HttpGet]
+        [Route("corvinus/nagybetus/{szoveg}")]
+        public IActionResult M2(string szoveg)
+        {
+            return new ContentResult
+            {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain, //"text/plain"
+                Content = szoveg.ToUpper()
+            };
+            return BadRequest("Nem jó a bemenő adat");
+        }
+    }
+}
